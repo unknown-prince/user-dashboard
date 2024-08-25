3bdb7a584da7a262d82db9e2058344cb394c4bd7
dominic-arendse <dominic.a@charted.co.za>
2024-08-25 11:15:32 +0200

    Backend test init

---
+++ services/backend/tests/go.mod
@@ -0,0 +1,3 @@
+module main_test
+
+go 1.23.0
---
+++ services/backend/tests/main_test.go
@@ -0,0 +1,9 @@
+package main_test
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	// @TODO once proper framework use in place then write this primary test for the webserver
+}
