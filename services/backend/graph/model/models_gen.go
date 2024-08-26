b18e9130034ad18b9d097c619baa7becadffaf13
dominic-arendse <dominic.a@charted.co.za>
2024-08-26 11:34:30 +0200

    user model schema setup and as well as dummy resolver

--- services/backend/graph/model/models_gen.go
+++ services/backend/graph/model/models_gen.go
@@ -2,25 +2,16 @@
 
 package model
 
-type Mutation struct {
-}
-
-type NewTodo struct {
-	Text   string `json:"text"`
-	UserID string `json:"userId"`
-}
-
 type Query struct {
 }
 
-type Todo struct {
-	ID   string `json:"id"`
-	Text string `json:"text"`
-	Done bool   `json:"done"`
-	User *User  `json:"user"`
-}
-
 type User struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	Surname    string `json:"surname"`
+	Number     int    `json:"number"`
+	Gender     string `json:"gender"`
+	Country    string `json:"country"`
+	Dependents int    `json:"dependents"`
+	Birthdate  string `json:"birthdate"`
 }
