dd2d12e104534bba38f218dc8923e5a7270d3dd2
dominic-arendse <dominic.a@charted.co.za>
2024-08-26 23:52:22 +0200

    seeder for users table

---
+++ services/backend/database/seeders/mysql/000001_seed_users_table.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO users
+VALUES
+(1, 'Jack', 'Front', 123, 'Male', 'Latvia', 5, '1981-10-03'),
+(2, 'Jill', 'Human', 654, 'Female', 'Spain', 0, '1983-06-02'),
+(3, 'Robert', 'Pullman', 456, 'Male', 'German', 2, '1999-05-04'),
+(4, 'Chun Li', 'Suzuki', 987, 'Female', 'China', 1, '2001-11-09'),
+(5, 'Sarah', 'Van Que', 587, 'Female', 'Latvia', 4, '1989-06-22'),
