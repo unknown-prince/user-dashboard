8d9ba383b8ed5148ea4d5323a3f894b2c0f82ed8
dominic-arendse <dominic.a@charted.co.za>
2024-08-27 04:47:06 +0200

    react and tailwind navbar implementation

--- services/frontend/app/page.js
+++ services/frontend/app/page.js
@@ -1,25 +1,3 @@
-import Image from "next/image";
-
-export default async function Home() {
-  const {status} = await fetch("http://localhost:8000").then(x => x.json());
-  const {version} = await fetch("http://localhost:8000/version").then(x => x.json());
-  const {time} = await fetch("http://localhost:8000/time").then(x => x.json());
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Backend Status:&nbsp;
-          <code className="font-mono font-bold">{status}</code>
-        </p>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Application Version:&nbsp;
-          <code className="font-mono font-bold">{version}</code>
-        </p>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Server Time:&nbsp;
-          <code className="font-mono font-bold">{time}</code>
-        </p>
-      </div>
-    </main>
-  );
+export default function Home() {
+  return <h1>Hello, Home page!</h1>
 }
