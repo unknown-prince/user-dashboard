60c43d25ea9a0b46ef7a528d131469e53367bcf2
dominic-arendse <dominic.a@charted.co.za>
2024-08-27 09:09:21 +0200

    standardise view

--- services/frontend/app/page.js
+++ services/frontend/app/page.js
@@ -6,7 +6,7 @@ export default function Page() {
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">Users</h1>
         </div>
       </header>
-      <main>
+      <main className="m-4">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">yep</div>
       </main>
     </div>
